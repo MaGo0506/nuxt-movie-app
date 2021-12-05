a158cc61efc58ca2a68bc6c14c7630ae47d1ce46
Marko Goronjic <goronjicmarko24@gmail.com>
2021-12-05 20:24:51 +0100

    * File Clean up

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/styles/app.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
