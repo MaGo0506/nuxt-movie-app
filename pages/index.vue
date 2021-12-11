c2398b3b7eb3bd22c7aa7b64b3e701fdfe6cd9e1
Marko Goronjic <goronjicmarko24@gmail.com>
2021-12-11 15:12:44 +0100

    Fetch Hook(Get Movie data)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
  <div class="home">
+   <!-- Hero -->
    <Hero/>
+
  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=ba7f48d120906a59bb4c8fccc134ff75&language=en-US&page=1')
+      const response = await data;
+      response.data.results.forEach(movie => {
+        this.movies.push(movie);
+      })
+      console.log(this.movies)
+    }
+  }
+}
 </script>
